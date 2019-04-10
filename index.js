c7627e07ecd76d3166792e63e1f1e06fd896a84e
krizk <cacollins08@gmail.com>
2019-04-10 18:10:59 -0400

    added html attribute bindin

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 new Vue({
-    el: '#app',   // el tells vue.js which element in your dom (id="app" in the HTML) you want to control through vue.js. This will become the template of this 'new' Vue instance which is then rendered by vue.js
+    el: '#app1',   // el tells vue.js which element in your dom (id="app" in the HTML) you want to control through vue.js. This will become the template of this 'new' Vue instance which is then rendered by vue.js
     data: {      // in the data object we store all the data we want to work with in this view instance
         title: 'hello world!'
     },
@@ -7,5 +7,12 @@ new Vue({
         changeTitle() {
              this.title = 'this is the new title'; // 'this' gives you access to all data within the instance of the Vue object
         }
+    }
+});
+
+new Vue({
+    el: '#app2',  
+    data: {      
+        cssClass: 'no class...'
     }
 });
